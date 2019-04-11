a7cafdf6a5471c76898dfb5d64c7e2bde78d6bbe
Ристевски Петар <petar.ristevski@students.finki.ukim.mk>
2019-04-11 21:31:02 +0200

    Initial commit

---
+++ src/main/java/com/petar/ristevski/todo/rest/TodoRestController.java
@@ -0,0 +1,29 @@
+package com.petar.ristevski.todo.rest;
+
+import java.util.List;
+
+import com.petar.ristevski.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.petar.ristevski.model.Todo;
+
+@RestController
+public class TodoRestController {
+	@Autowired
+	private TodoService service;
+
+	@RequestMapping(value = "/todo/", method = RequestMethod.GET)
+	public List<Todo> listAllTodos() {
+		List<Todo> users = service.retrieveTodos("in28Minutes");
+		return users;
+	}
+
+	@RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
+	public Todo retrieveTodo(@PathVariable("id") int id) {
+		return service.retrieveTodo(id);
+	}
+}
